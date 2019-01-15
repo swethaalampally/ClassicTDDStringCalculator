14891db800292999b039a40acdeed491a27a90a5
Swetha Alampally <Swetha@villaplus.com>
2019-01-15 13:30:18 +0000

    refactoring the test code

--- ClassicTDDStringCalculator/UnitTest1.cs
+++ ClassicTDDStringCalculator/UnitTest1.cs
@@ -5,13 +5,14 @@ namespace ClassicTDDStringCalculator
 {
     public class UnitTest1
     {
-        [Fact]
-        public void shouldreturnaddition()
+        [Theory]
+        [InlineData("",0)]
+        public void shouldreturnaddition(string numbers,int expectedValue)
         {
             StringCalculator obj = new StringCalculator();
-            int sum = obj.add("");
+            int sum = obj.add(numbers);
 
-            Assert.Equal(0, sum);
+            Assert.Equal(expectedValue, sum);
 
         }
     }
